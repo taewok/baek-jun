f4f45f01971140f4165bc53fea88f9ff0366e4d8
taewok <taewok5163@naver.com>
2022-11-04 21:20:43 +0900

    구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력 일치하지 않는다면 No

--- 25304번/index.js
+++ 25304번/index.js
@@ -4,12 +4,12 @@ const inputData = fs.readFileSync("input.txt").toString().split("\n");
 const total = parseInt(inputData[0]);
 const count = parseInt(inputData[1])
 const priceList = inputData.slice(2,2+count);
-let sum;
+let sum=0;
 for (let index = 0; index < count; index++) {
     let a = priceList[index].split(" ")
     sum += parseInt(a[0]) * parseInt(a[1]);
 }
-if (sum = total) {
+if (sum == total) {
     console.log("Yes")
 }else{
     console.log("No")
